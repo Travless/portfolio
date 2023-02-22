a9e8411b745701d68f29030e2daef92774581a28
Travless <travis.white0101@gmail.com>
2023-02-22 14:25:01 -0500

    Add bouncing hover animation over each letter in name and title sectiosn of Homepage component

--- my-portfolio/src/components/Home/index.js
+++ my-portfolio/src/components/Home/index.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['T', 'r', 'a', 'v', 'i', 's', ' ', 'W', 'h', 'i', 't', 'e'];
+    const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, []);
+
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi,<br /></h1>
-                <h1>I'm Travis White</h1>
-                <h1>Web Developer</h1>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass} 
+                    strArray={nameArray}
+                    idx={15}/>
+                </h1>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass} 
+                    strArray={jobArray}
+                    idx={22} />
+                </h1>
                 <h2>Frontend Developer with Full Stack Aspirations</h2>
                 <h2>Javascript | HTML | CSS | Node.js & more</h2>
                 <Link to='/contact' className="flat-button">CONTACT  ME</Link>
