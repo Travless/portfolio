c877a1f833bdf6f1f47e609f910798a05412504b
Travless <travis.white0101@gmail.com>
2023-02-22 12:39:23 -0500

    Develop Sidebar component to function as main navbar. Add baseline styling and animation

---
+++ my-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,65 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => {
+    return(
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoS} alt='logo' />
+                <img src={LogoSubtitle} alt='slobodan' className='sub-logo' />
+            </Link>
+            <nav>
+                <NavLink 
+                    exact='true' 
+                    activeclassname='active' 
+                    to='/' 
+                >
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact='true' 
+                    activeclassname='active' 
+                    to='/about' 
+                    className='about-link' 
+                >
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink 
+                    exact='true' 
+                    activeclassname='active' 
+                    to='/contact' 
+                    className='contact-link' 
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.linkedin.com'    
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.github.com/Travless'    
+                    >
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
